0ed51b455535b4206ee650d14e9452e4dcaf662b
Tihici <tihamer.sebok@gmail.com>
2020-04-07 09:47:48 +0300

    komponentek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
+import { FooterComponent } from './footer/footer.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventcardComponent,
+    FooterComponent,
+    JumbotronComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
