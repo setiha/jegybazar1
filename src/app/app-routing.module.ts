48c97b697124440bdfb55d780fe4c770332dd7d9
Tihici <tihamer.sebok@gmail.com>
2020-04-12 08:46:51 +0300

    ticket/user/event refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import {EventComponent} from './event/event.component';
 import {TicketComponent} from './ticket/ticket.component';
 import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {EventListComponent} from './event-list/event-list.component';
-import {EventDetailComponent} from './event-detail/event-detail.component';
-import {ProfileComponent} from './profile/profile.component';
-import {ProfileEditComponent} from './profile-edit/profile-edit.component';
-import {TicketListComponent} from "./ticket-list/ticket-list.component";
-import {TicketDetailsComponent} from "./ticket-details/ticket-details.component";
+import {LoginComponent} from './user/login/login.component';
+import {RegistrationComponent} from './user/registration/registration.component';
+import {EventListComponent} from './event/event-list/event-list.component';
+import {EventDetailComponent} from './event/event-detail/event-detail.component';
+import {ProfileComponent} from './user/profile/profile.component';
+import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
+import {TicketListComponent} from './ticket/ticket-list/ticket-list.component';
+import {TicketDetailsComponent} from './ticket/ticket-details/ticket-details.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -22,7 +22,7 @@ const routes: Routes = [
       [
         {path: 'list', component: EventListComponent},
         {path: 'new', component: EventDetailComponent},
-        {path: ':id/edit', component: EventDetailComponent},
+        {path: ':id', component: EventDetailComponent},
         ]
   },
   {path: 'about', component: AboutComponent},
