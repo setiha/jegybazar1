804660adf43276c008748892bbd2f6b18128713b
Tihici <tihamer.sebok@gmail.com>
2020-04-14 12:50:28 +0300

    a gisthez kell

--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {EventModel} from './event-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+private _events: EventModel[];
+  constructor() {
+    this._events = [
+      {
+        id: 1,
+        name: 'Sziget Fesztivál',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'img/sziget.png',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 2,
+        name: 'Sziget Fesztivál',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'img/sziget.png',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      }
+    ];
+  }
 
-  constructor() { }
+  getAllEvents(): EventModel[]{
+    return this._events;
+  }
 }
