55ef8c3633d216f298950a37d6fd3b842a7cb3e9
Tihici <tihamer.sebok@gmail.com>
2020-04-15 17:46:41 +0300

    9 esemeny bekotve

--- src/app/event/eventcard/eventcard.component.ts
+++ src/app/event/eventcard/eventcard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {EventModel} from "../../shared/event-model";
+
 
 @Component({
   selector: 'app-eventcard',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventcard.component.css']
 })
 export class EventcardComponent implements OnInit {
-
+  @Input() esemeny: EventModel;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -12,14 +12,63 @@ private _events: EventModel[];
         id: 1,
         name: 'Sziget Fesztivál',
         date: '2018-08-03T18:00:00',
-        pictureURL: 'img/sziget.png',
+        pictureURL: 'assets/sziget.png',
         description: 'Nézd meg a grandiőzus balett előadást most elővételben'
       },
       {
         id: 2,
+        name: 'Macskak',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/macskak.jpg',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 3,
+        name: 'Diotoro',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/diotoro.jpg',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 4,
         name: 'Sziget Fesztivál',
         date: '2018-08-03T18:00:00',
-        pictureURL: 'img/sziget.png',
+        pictureURL: 'assets/sziget.png',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 5,
+        name: 'Macskak',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/macskak.jpg',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 6,
+        name: 'Diotoro',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/diotoro.jpg',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 7,
+        name: 'Sziget Fesztivál',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/sziget.png',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 8,
+        name: 'Macskak',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/macskak.jpg',
+        description: 'Nézd meg a grandiőzus balett előadást most elővételben'
+      },
+      {
+        id: 9,
+        name: 'Diotoro',
+        date: '2018-08-03T18:00:00',
+        pictureURL: 'assets/diotoro.jpg',
         description: 'Nézd meg a grandiőzus balett előadást most elővételben'
       }
     ];
